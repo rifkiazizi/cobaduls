7fd2ffba20768533a60934658ec72b8ac5155340
rifkiazizi <rifkiazizi00@gmail.com>
2020-03-21 13:06:45 +0700

    Laravel

---
+++ cobaLaravel/resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layout/main')
+
+@section('title', 'web programming tegal')
+
+
+@section(container)
+<div class="container">
+    <div class="row">
+        <div class="colloum">
+    <h1 class="mt-3">Hello, tegal!</h1>
+        </div>
+   </div>
+</div>
+
+@endsection
